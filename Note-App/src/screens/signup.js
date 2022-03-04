fee895af3adfde29bfaad8fb00b170f416324bf2
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-03-04 20:45:30 +0600

    Update signup.js

--- Note-App/src/screens/signup.js
+++ Note-App/src/screens/signup.js
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Input from '../components/Input'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Signup() {
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{margin: 25}}>
+         <Input placeholder="Email" />
+         <Input placeholder="Password" />
+         <Input placeholder="Full name" />
+         <Input placeholder="Age" />
+      </View>
+    </SafeAreaView>
   )
 }
