e6e46d089e8a5221b8eabbdddfef45db3d37ab47
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-03-04 09:21:46 +0600

    update app.js

--- Note-App/App.js
+++ Note-App/App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/home';
+import Create from './src/screens/create';
+import Update from './src/screens/update';
+import Login from './src/screens/login';
+import Signup from './src/screens/signup';
+import React from 'react';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [user, setUser] = React.useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {
+          user ? (
+          <>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Create" component={Create} />
+            <Stack.Screen name="Update" component={Update} />
+          </>
+        ) : (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+        </>
+        )
+        }
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
