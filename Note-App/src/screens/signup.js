781362c7f271b7fde7ff08d2126ff18fbb13b073
Marzan Islam <marzanislam999@gmail.com>
2022-04-06 12:06:47 +0600

    connect with firebase

--- Note-App/src/screens/signup.js
+++ Note-App/src/screens/signup.js
@@ -4,6 +4,8 @@ import Input from '../components/input'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import RadioInput from '../../src/components/radio-input'
 import Button from '../components/button'
+import { firebase } from '../config'
+import { ActivityIndicator } from 'react-native'
 
 const OPTIONS = ['Male', 'Female']
 
@@ -13,35 +15,61 @@ export default function Signup() {
   const [password, setPassword] = React.useState('')
   const [name, setName] = React.useState('')
   const [age, setAge] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
+
+  const signup = () => {
+
+    setLoading(true)
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log('response --- ', response)
+      }).catch((error) => {
+        setLoading(false)
+        console.log('error ', error)
+      }
+      );
+
+    }
 
 
-  return (
-    <SafeAreaView>
-      <View style={{ margin: 25 }}>
-        <Input placeholder="Email" onChangeText={(text) => setEmail(text)} />
-        <Input
-          placeholder="Password"
-          onChangeText={(text) => setPassword(text)}
-          secureTextEntry={true}
-        />
-        <Input placeholder="Full name" onChangeText={(text) => setName(text)} />
-        <Input placeholder="Age" onChangeText={(text) => setAge(text)} />
+    return (
+      <SafeAreaView>
+        <View style={{ margin: 25 }}>
+          <Input placeholder="Email" onChangeText={(text) => setEmail(text)} />
+          <Input
+            placeholder="Password"
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+          />
+          <Input placeholder="Full name" onChangeText={(text) => setName(text)} />
+          <Input placeholder="Age" onChangeText={(text) => setAge(text)} />
 
-        <View style={{ marginTop: 20 }}>
-          <Text style={{ marginBottom: 20 }}>Select your gender</Text>
-          {OPTIONS.map((option, index) => (
-            <RadioInput
-              key={index}
-              lebel={option}
-              value={gender}
-              setValue={setGender}
+          <View style={{ marginTop: 20 }}>
+            <Text style={{ marginBottom: 20 }}>Select your gender</Text>
+            {OPTIONS.map((option, index) => (
+              <RadioInput
+                key={index}
+                lebel={option}
+                value={gender}
+                setValue={setGender}
+              />
+            ))}
+          </View>
+          {loading ?
+            <ActivityIndicator /> :
+            <Button
+              title="submit"
+              customStyle={{ marginTop: 25, alignSelf: 'center' }}
+              onPress={signup}
             />
-          ))}
+          }
+
+
+
         </View>
-
-        <Button title="submit" customStyle={{ marginTop: 25, alignSelf: 'center' }} />
-
-      </View>
-    </SafeAreaView>
-  )
-}
+      </SafeAreaView>
+    )
+  }
