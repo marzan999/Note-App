6bf605dc7b856d3502e8dee2f00501af73983f36
Marzan Islam <marzanislam999@gmail.com>
2022-04-06 20:54:39 +0600

    Authentication & Firestore Database

--- Note-App/src/screens/signup.js
+++ Note-App/src/screens/signup.js
@@ -26,50 +26,67 @@ export default function Signup() {
       .createUserWithEmailAndPassword(email, password)
       .then((response) => {
         console.log('response --- ', response)
+
+        const uid = response.user.uid
+
+
+        const userProfileData = {
+          id: uid,
+          name: name,
+          age: age,
+          email: email,
+          gender: gender,
+        }
+
+const userRef = firebase.firestore().collection("users");
+
+userRef.doc(uid).set(userProfileData);
+
+        setLoading(false)
       }).catch((error) => {
         setLoading(false)
         console.log('error ', error)
       }
       );
 
-    }
-
-
-    return (
-      <SafeAreaView>
-        <View style={{ margin: 25 }}>
-          <Input placeholder="Email" onChangeText={(text) => setEmail(text)} />
-          <Input
-            placeholder="Password"
-            onChangeText={(text) => setPassword(text)}
-            secureTextEntry={true}
-          />
-          <Input placeholder="Full name" onChangeText={(text) => setName(text)} />
-          <Input placeholder="Age" onChangeText={(text) => setAge(text)} />
-
-          <View style={{ marginTop: 20 }}>
-            <Text style={{ marginBottom: 20 }}>Select your gender</Text>
-            {OPTIONS.map((option, index) => (
-              <RadioInput
-                key={index}
-                lebel={option}
-                value={gender}
-                setValue={setGender}
-              />
-            ))}
-          </View>
-          {loading ?
-            <ActivityIndicator /> :
-            <Button
-              title="submit"
-              customStyle={{ marginTop: 25, alignSelf: 'center' }}
-              onPress={signup}
-            />
-          }
-
-
-
-        </View>
-      </SafeAreaView>
-    )
   }
+
+
+  return (
+    <SafeAreaView>
+      <View style={{ margin: 25 }}>
+        <Input placeholder="Email" onChangeText={(text) => setEmail(text)} />
+        <Input
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+        />
+        <Input placeholder="Full name" onChangeText={(text) => setName(text)} />
+        <Input placeholder="Age" onChangeText={(text) => setAge(text)} />
+
+        <View style={{ marginTop: 20 }}>
+          <Text style={{ marginBottom: 20 }}>Select your gender</Text>
+          {OPTIONS.map((option, index) => (
+            <RadioInput
+              key={index}
+              lebel={option}
+              value={gender}
+              setValue={setGender}
+            />
+          ))}
+        </View>
+        {loading ?
+          <ActivityIndicator /> :
+          <Button
+            title="submit"
+            customStyle={{ marginTop: 25, alignSelf: 'center' }}
+            onPress={signup}
+          />
+        }
+
+
+
+      </View>
+    </SafeAreaView>
+  )
+}
