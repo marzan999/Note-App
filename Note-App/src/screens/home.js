5484da84311d672968746d4fe70849f86d2ddfe0
Marzan Islam <marzanislam999@gmail.com>
2022-04-11 16:31:20 +0600

    home decoration

--- Note-App/src/screens/home.js
+++ Note-App/src/screens/home.js
@@ -1,8 +1,34 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { firebase } from '../config'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+
+const slides = [
+  {
+    key: 1,
+    title: 'Document',
+    subtitle: 'Make yourself better',
+    text: 'Learn new stuffs get professional',
+    image: require('../../assets/banner1.png'),
+  },
+  {
+    key: 2,
+    title: 'Learn',
+    subtitle: 'Learn and grow',
+    text: 'You can earn professionally with our bootcamps',
+    image: require('../../assets/banner2.png'),
+  },
+  {
+    key: 3,
+    title: 'Help',
+    subtitle: 'Help others by educating',
+    text: 'Create your own notes and help others',
+    image: require('../../assets/banner3.png'),
+  }
+];
 
 const Onboarding = () => {
   const makeOnboardingTrue = async () => {
@@ -17,8 +43,28 @@ const Onboarding = () => {
     makeOnboardingTrue()
   }, [])
 
+  const renderItem = ({ item }) => {
+
+    const { key, title, subtitle, text, image } = item 
+    return (
+      <View style={{flex: 1 }}>
+      <Image source={image} style={{width: '100%', height: 300 }} resizeMode="contain"/>
+      </View>
+    )
+  }
+
+  const onDone = () => {
+
+  }
+
   return (
-    <Text>Onboarding</Text>
+    <AppIntroSlider
+      renderItem={renderItem}
+      data={slides}
+      onDone={onDone}
+      keyExtractor={item => item.key}
+      activeDotStyle={{backgroundColor: 'green'}}
+    />
   )
 
 }
@@ -47,6 +93,12 @@ export default function Home() {
   React.useEffect(() => {
     getOnboardingValue()
   }, []);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Onboarding/>
+    </SafeAreaView>
+  )
 
   if (checking) {
     return null
